a3d9287add79740eb9faa5e42c95d0750fd582df
Alzahraa El-Sallakh <zahraaelsallakh@gmail.com>
2020-04-25 06:15:34 +0200

    Appending Compiler.h and Compiler_Cfg.h v1.0.0

--- Dio/Files/Compiler.h
+++ Dio/Files/Compiler.h
@@ -1,7 +1,149 @@
+/**
+ * @file Compiler.h
+ * @author Mark & May
+ * @brief This is the user interface for the compiler
+ * @version 1.0.0
+ * @date 2020-04-25
+ * 
+ * SPECIFICATION(S) : Compiler Abstraction, AUTOSAR Release 4.3.1
+ * @copyright Copyright (c) 2020
+ * 
+ */
+
 #ifndef COMPILER_H
 #define COMPILER_H
+#include "Compiler_Cfg.h"
 
+/* For Automatic Type */
+#define AUTOMATIC
+/* For Defined Data Types */
+#define TYPEDEF
+/* Null Pointer */
+#define NULL_PTR                                        ((void*)0)
+/* Inline */
+#define INLINE                                          inline
+/* Local Inline */
+#define LOCAL_INLINE                                    static inline
 
+/**
+ * @brief A function Difinition
+ * 
+ * @param rettype The return type of the function
+ * @param memclass The memory class
+ *              @arg far
+ *              @arg near
+ * 
+ */
+#define FUNC(rettype, memclass)                         rettype
 
+/**
+ * @brief A Function that returns a pointer to a constant
+ * @param rettype The return type of the function
+ * @param ptrclass The pointer class
+ *              @arg far
+ *              @arg near
+ * @param memclass The memory class
+ *              @arg far
+ *              @arg near
+ */
+#define FUNC_P2CONST(rettype, ptrclass, memclass)       const rettype *
 
-#endif /* COMPILER_H */
+/**
+ * @brief A Function that returns a pointer to a variable
+ * @param rettype The return type of the function
+ * @param ptrclass The pointer class
+ *              @arg far
+ *              @arg near
+ * @param memclass The memory class
+ *              @arg far
+ *              @arg near
+ */
+#define FUNC_P2VAR(rettype, ptrclass, memclass)         rettype * 
+
+/**
+ * @brief A pointer to a variable
+ * @param ptrtype The type of the pointer
+ * @param ptrclass The pointer class
+ *              @arg far
+ *              @arg near
+ * @param memclass The memory class
+ *              @arg far
+ *              @arg near
+ */
+#define P2VAR(ptrtype, memclass, ptrclass)              ptrtype *
+
+/**
+ * @brief A pointer to a constant variable
+ * @param ptrtype The type of the pointer
+ * @param ptrclass The pointer class
+ *              @arg far
+ *              @arg near
+ * @param memclass The memory class
+ *              @arg far
+ *              @arg near
+ */
+#define P2CONST(ptrtype, memclass, ptrclass)            const ptrtype *
+
+/**
+ * @brief A constant pointer to a variable
+ * @param ptrtype The type of the pointer
+ * @param ptrclass The pointer class
+ *              @arg far
+ *              @arg near
+ * @param memclass The memory class
+ *              @arg far
+ *              @arg near
+ */
+#define CONSTP2VAR(ptrtype, memclass, ptrclass)         ptrtype * const 
+
+/**
+ * @brief A constant pointer to a constant variable
+ * @param ptrtype The type of the pointer
+ * @param ptrclass The pointer class
+ *              @arg far
+ *              @arg near
+ * @param memclass The memory class
+ *              @arg far
+ *              @arg near
+ */
+#define CONSTP2CONST(ptrtype, memclass, ptrclass)       const ptrtype * const 
+
+/**
+ * @brief A pointer to a function
+ * @param rettype The return type of the function
+ * @param ptrclass The pointer class
+ *              @arg far
+ *              @arg near
+ * @param fctname the function name
+ */
+#define P2FUNC(rettype, ptrclass, fctname)              rettype (*funcname)
+
+/**
+ * @brief A constant pointer to a function
+ * @param rettype The return type of the function
+ * @param ptrclass The pointer class
+ *              @arg far
+ *              @arg near
+ * @param fctname the function name
+ */
+#define CONSTP2FUNC(rettype, ptrclass, fctname)         rettype (* const funcname)
+
+/**
+ * @brief A constant variable
+ * @param consttype the type of the constant variable
+ * @param memclass The memory class
+ *              @arg far
+ *              @arg near
+ */
+#define CONST(consttype, memclass)                      const consttype 
+
+/**
+ * @brief A variable
+ * @param vartype the type of the variable
+ * @param memclass The memory class
+ *              @arg far
+ *              @arg near
+ */
+#define VAR(vartype, memclass)                          vartype
+
+#endif
---
+++ Dio/Files/Compiler_Cfg.h
@@ -0,0 +1,27 @@
+/**
+ * @file Compiler_Cfg.h
+ * @author Mark & May
+ * @brief These are the configurations for compiler
+ * @version 1.0.0
+ * @date 2020-04-25
+ * 
+ * SPECIFICATION(S) : Compiler Abstraction, AUTOSAR Release 4.3.1 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+#ifndef COMPILER_CFG_H
+#define COMPILER_CFG_H
+
+#define REGSPACE
+
+#define DIO_CODE
+#define DIO_CODE_FAST
+#define DIO_CODE_SLOW
+#define DIO_CONST
+#define DIO_CALIB
+#define DIO_CONFIG_DATA
+#define DIO_CONST_SAVED_RECOVERY_ZONE
+#define DIO_APPL_DATA
+#define DIO_APPL_CONST
+
+#endif
